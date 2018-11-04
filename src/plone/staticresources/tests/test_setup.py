cccdf3db36026b38cd5f25ab2993d13a9d694190
Johannes Raggam <thetetet@gmail.com>
2018-11-05 00:13:04 +0100

    Add uninstall profile - remove most resources but no bundles for the moment.

--- src/plone/staticresources/tests/test_setup.py
+++ src/plone/staticresources/tests/test_setup.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
+from plone.registry.interfaces import IRegistry
 from plone.staticresources.testing import PLONE_STATICRESOURCES_INTEGRATION_TESTING  # noqa
 from zope.component import getMultiAdapter
+from zope.component import getUtility
 
 import unittest
 
@@ -15,23 +17,55 @@ class TestSetup(unittest.TestCase):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
         self.request = self.layer['request']
-
-    def test_install_uninstall(self):
-        """Test if plone.staticresources is installed and is able to be
-        uninstalled."""
-
-        installer = getMultiAdapter(
+        self.installer = getMultiAdapter(
             (self.portal, self. request),
             name=u'prefs_install_products_form'
         )
+        self.registry = getUtility(IRegistry)
 
-        # Install
-        self.assertTrue(installer.is_product_installed(
+    def test_install(self):
+        """Test if plone.staticresources is installed."""
+        self.assertTrue(self.installer.is_product_installed(
             'plone.staticresources')
         )
 
-        # Uninstall
-        installer.uninstall_product('plone.staticresources')
-        self.assertFalse(installer.is_product_installed(
+        # Test availability of bundles and resources
+        self.assertEqual(
+            self.registry.records.get('plone.resources/resourceregistry.js').value,  # noqa
+            '++plone++static/resourceregistry.js'
+        )
+
+        self.assertEqual(
+            self.registry.records.get('plone.resources/less.js').value,
+            '++plone++static/components/less/dist/less.js'
+        )
+
+        self.assertEqual(
+            self.registry.records.get('plone.resources/jquery.js').value,
+            '++plone++static/components/jquery/dist/jquery.min.js'
+        )
+
+    def test_uninstall(self):
+        """Test if plone.staticresources can be uninstalled."""
+        self.installer.uninstall_product('plone.staticresources')
+        self.assertFalse(self.installer.is_product_installed(
             'plone.staticresources')
+        )
+
+        # We keep all bundles - at least this one should be available.
+        self.assertEqual(
+            self.registry.records.get('plone.resources/resourceregistry.js').value,  # noqa
+            '++plone++static/resourceregistry.js'
+        )
+
+        # This one is removed
+        self.assertEqual(
+            self.registry.records.get('plone.resources/less.js'),
+            None
+        )
+
+        # jquery is also kept is it's still necessary for Plone core.
+        self.assertEqual(
+            self.registry.records.get('plone.resources/jquery.js').value,
+            '++plone++static/components/jquery/dist/jquery.min.js'
         )
