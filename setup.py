23b72361b19e52d7f43ea357c5ce40371a953172
esteele <eric@esteele.net>
2019-03-12 20:44:08 -0400

    Back to development: 1.0.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='plone.staticresources',
-    version='1.0.1',
+    version='1.0.2.dev0',
     description="Static resources for Plone",
     long_description=long_description,
     classifiers=[
