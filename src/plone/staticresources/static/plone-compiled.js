a400923e088f8e8e3bbb651b8366ba553bfab093
Peter Holzer <peter.holzer@agitator.com>
2019-06-18 09:43:42 +0200

    Integrate https://github.com/plone/mockup/pull/906 which fixes wrong in-path marking for similar pathnames.

--- src/plone/staticresources/static/plone-compiled.js
+++ src/plone/staticresources/static/plone-compiled.js
@@ -18148,18 +18148,18 @@ define(
 define('mockup-patterns-navigationmarker',[
     'jquery',
     'pat-base'
-], function($, Base) {
+], function ($, Base) {
 
     var Navigation = Base.extend({
         name: 'navigationmarker',
         trigger: '.pat-navigationmarker',
         parser: 'mockup',
-        init: function() {
+        init: function () {
             var self = this;
             var href = document.querySelector('head link[rel="canonical"]').href || window.location.href;
 
             $('a', this.$el).each(function () {
-                var navlink = this.href.replace('/view','')
+                var navlink = this.href.replace('/view', '');
                 if (href.indexOf(navlink) !== -1) {
                     var parent = $(this).parent();
 
@@ -18170,7 +18170,21 @@ define('mockup-patterns-navigationmarker',[
                     }
 
                     // set "inPath" to all nav items which are within the current path
-                    parent.addClass('inPath');
+                    // check if parts of navlink are in canonical url parts
+                    var hrefParts = href.split('/');
+                    var navParts = navlink.split('/');
+                    var inPath = false;
+                    for (var i = 0, size = navParts.length; i < size; i++) {
+                        // The last path-part must match.
+                        inPath = false;
+                        if (navParts[i] === hrefParts[i]) {
+                            inPath = true;
+                        }
+                    }
+                    if (inPath) {
+                        parent.addClass('inPath');
+                    }
+
                     // set "current" to the current selected nav item, if it is in the navigation structure.
                     if (href === navlink) {
                         parent.addClass('current');
@@ -19277,5 +19291,5 @@ require([
 
 });
 
-define("/Users/esteele/projects/plone_5.2/src/plone.staticresources/src/plone/staticresources/static/plone.js", function(){});
+define("/home/_thet/data/dev/plone/buildout.coredev/src/plone.staticresources/src/plone/staticresources/static/plone.js", function(){});
 
