b33e578a6cbdb89c4716d2c3c0a6f16881ca0605
Johannes Raggam <thetetet@gmail.com>
2020-02-11 16:12:52 +0100

    Code formating according to Plone standards (black, isort)

--- src/plone/staticresources/_scripts/compile_resources.py
+++ src/plone/staticresources/_scripts/compile_resources.py
@@ -23,146 +23,140 @@ package_json_contents = """{
   }
 }"""  # noqa
 
-if os.name == 'nt':
-    NPM_CMD = 'npm.cmd'
+if os.name == "nt":
+    NPM_CMD = "npm.cmd"
 else:
-    NPM_CMD = 'npm'
+    NPM_CMD = "npm"
 
 
 def generate_package_json(base_path):
     # generate package.json file here if not exists...
-    package_json = os.path.join(base_path, 'package.json')
+    package_json = os.path.join(base_path, "package.json")
     if not os.path.exists(package_json):
-        with open(package_json, 'w') as fi:
+        with open(package_json, "w") as fi:
             fi.write(package_json_contents)
 
 
 def generate_gruntfile(base_path, instance, site_id, compile_dir):
     if not instance:
         # look for it, get bin directory, search for plone instance
-        bin_path = os.path.join(base_path, 'bin')
+        bin_path = os.path.join(base_path, "bin")
         files = os.listdir(bin_path)
-        if 'instance' in files:
-            instance = os.path.join(bin_path, 'instance')
-        elif 'client1' in files:
-            instance = os.path.join(bin_path, 'client1')
+        if "instance" in files:
+            instance = os.path.join(bin_path, "instance")
+        elif "client1" in files:
+            instance = os.path.join(bin_path, "client1")
     if not instance:
-        print('Could not find plone instance to run command against.')
+        print("Could not find plone instance to run command against.")
         sys.exit()
-    script_path = os.path.join(this_dir, '_generate_gruntfile.py')
-    cmd = [instance, 'run', script_path]
-    os.environ['SITE_ID'] = site_id
-    os.environ['COMPILE_DIR'] = compile_dir
-    print('Running command: %s' % ' '.join(cmd))
+    script_path = os.path.join(this_dir, "_generate_gruntfile.py")
+    cmd = [instance, "run", script_path]
+    os.environ["SITE_ID"] = site_id
+    os.environ["COMPILE_DIR"] = compile_dir
+    print("Running command: %s" % " ".join(cmd))
     subprocess.check_call(cmd, env=os.environ)
-    print('Gruntfile generated.')
+    print("Gruntfile generated.")
 
 
 def main(argv=sys.argv):
     parser = argparse.ArgumentParser(
-        description='Generate and setup Grunt infrastructure, '
-        'then compile JS/LESS bundles for Plone.',
+        description="Generate and setup Grunt infrastructure, "
+        "then compile JS/LESS bundles for Plone.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        '-i',
-        '--instance',
-        dest='instance',
-        help='path to instance executable. If not provided, '
-        'will look in bin this was executed from for '
-        'instance or client1',
+        "-i",
+        "--instance",
+        dest="instance",
+        help="path to instance executable. If not provided, "
+        "will look in bin this was executed from for "
+        "instance or client1",
     )
     parser.add_argument(
-        '-s',
-        '--site-id',
-        dest='site_id',
-        default='Plone',
-        help='ID of the Plone site to fetch the configuration from. '
-        'Used only while Gruntfile generation.',
+        "-s",
+        "--site-id",
+        dest="site_id",
+        default="Plone",
+        help="ID of the Plone site to fetch the configuration from. "
+        "Used only while Gruntfile generation.",
     )
     parser.add_argument(
-        '-b',
-        '--bundle',
-        dest='bundle',
-        default='all',
-        help='Name of bundle to compile. Used while compile step.',
+        "-b",
+        "--bundle",
+        dest="bundle",
+        default="all",
+        help="Name of bundle to compile. Used while compile step.",
     )
     parser.add_argument(
-        '--compile-dir',
-        dest='compile_dir',
-        default='',
-        help='Output directory for the compiled bundle files. '
-        'Used only while Gruntfile generation. '
-        'If not given the directory is looked up from Plone registry. ',
+        "--compile-dir",
+        dest="compile_dir",
+        default="",
+        help="Output directory for the compiled bundle files. "
+        "Used only while Gruntfile generation. "
+        "If not given the directory is looked up from Plone registry. ",
     )
     parser.add_argument(
-        '-d',
-        '--base-dir',
-        dest='base_dir',
-        default='.',
-        help='Base directory for this script '
-        '(by default current working directory).',
+        "-d",
+        "--base-dir",
+        dest="base_dir",
+        default=".",
+        help="Base directory for this script "
+        "(by default current working directory).",
     )
     parser.add_argument(
-        '-G',
-        '--skip-generate-gruntfile',
-        dest='skip_gruntfile',
-        action='store_true',
-        help='Skip generation of Gruntfile.js',
+        "-G",
+        "--skip-generate-gruntfile",
+        dest="skip_gruntfile",
+        action="store_true",
+        help="Skip generation of Gruntfile.js",
     )
     parser.add_argument(
-        '-I',
-        '--skip-npm-install',
-        dest='skip_npminstall',
-        action='store_true',
-        help='Skip npm install step',
+        "-I",
+        "--skip-npm-install",
+        dest="skip_npminstall",
+        action="store_true",
+        help="Skip npm install step",
     )
     parser.add_argument(
-        '-C',
-        '--skip-compile',
-        dest='skip_compile',
-        action='store_true',
-        help='Skip compile step (running grunt)',
+        "-C",
+        "--skip-compile",
+        dest="skip_compile",
+        action="store_true",
+        help="Skip compile step (running grunt)",
     )
 
     args = parser.parse_args()
 
     base_path = args.base_dir
-    if base_path == '.':
+    if base_path == ".":
         base_path = os.getcwd()
 
     # generates only if not already there
     generate_package_json(base_path)
     if not args.skip_npminstall:
-        cmd = [NPM_CMD, 'install']
-        print('Setup npm env')
-        print('Running command: %s' % ' '.join(cmd))
+        cmd = [NPM_CMD, "install"]
+        print("Setup npm env")
+        print("Running command: %s" % " ".join(cmd))
         subprocess.check_call(cmd)
 
     # Generate Gruntfile
-    grunt = os.path.join(
-        base_path, 'node_modules', 'grunt-cli', 'bin', 'grunt'
-    )
+    grunt = os.path.join(base_path, "node_modules", "grunt-cli", "bin", "grunt")
     if not args.skip_gruntfile:
-        generate_gruntfile(
-            base_path, args.instance, args.site_id, args.compile_dir
-        )
-    gruntfile = os.path.join(base_path, 'Gruntfile.js')
+        generate_gruntfile(base_path, args.instance, args.site_id, args.compile_dir)
+    gruntfile = os.path.join(base_path, "Gruntfile.js")
     if not os.path.exists(gruntfile):
         print(
-            'Error, no Gruntfile.js generated at {0} where expected'.format(
-                gruntfile
-            )
+            "Error, no Gruntfile.js generated at {0} where expected".format(gruntfile)
         )
 
     if not args.skip_compile:
-        print('Compile {0}'.format(args.bundle))
+        print("Compile {0}".format(args.bundle))
         cmd = [
             grunt,
-            '--gruntfile={0}'.format(gruntfile),
-            'compile-{0}'.format(args.bundle),
+            "--gruntfile={0}".format(gruntfile),
+            "compile-{0}".format(args.bundle),
         ]
-        print('Running command: %s' % ' '.join(cmd))
+        print("Running command: %s" % " ".join(cmd))
         subprocess.check_call(cmd)
 
-    print('Done.')
+    print("Done.")
