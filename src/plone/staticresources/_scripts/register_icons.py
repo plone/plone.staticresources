72af5010e8602e8213b43997a1833b60c3fd9d9a
Peter Holzer <peter.holzer@agitator.com>
2021-01-27 16:13:13 +0100

    update icon prefix and xml path

--- src/plone/staticresources/_scripts/register_icons.py
+++ src/plone/staticresources/_scripts/register_icons.py
@@ -4,7 +4,7 @@ import os
 this_dir = os.path.dirname(os.path.realpath(__file__))
 
 ICONS_DIR = "{dir}/../static/components/bootstrap-icons/icons".format(dir=this_dir)
-OUTPUT_FILE = "{dir}/../profiles/default/registry/bootstrap_icons.xml".format(
+OUTPUT_FILE = "{dir}/../profiles/default/registry/icons_bootstrap.xml".format(
     dir=this_dir
 )
 
@@ -19,7 +19,7 @@ entry = """
     <field type="plone.registry.field.TextLine">
       <title>Bootstrap Icon {icon}</title>
     </field>
-    <value key="resource">++plone++static/components/bootstrap-icons/icons/{icon}.svg</value>
+    <value key="resource">++plone++bootstrap-icons/{icon}.svg</value>
   </record>
 """
 
