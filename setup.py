16d72e44b50ab74b9c42ff84165860db1d969d4d
Johannes Raggam <thetetet@gmail.com>
2021-02-19 11:25:20 +0100

    Increase Python package version number to 2.0.dev0.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plone.staticresources",
-    version="1.4.1.dev0",
+    version="2.0.dev0",
     description="Static resources for Plone",
     long_description=long_description,
     classifiers=[
