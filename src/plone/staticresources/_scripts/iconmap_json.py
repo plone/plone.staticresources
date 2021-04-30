21a3ffbc0ae1c4015a33a930d1ed0b8dc9352dd0
Johannes Raggam <thetetet@gmail.com>
2021-04-30 11:48:04 +0200

    PLIP 3211 - Add iconmap.json and generator for usage as fallback in Mockup.

---
+++ src/plone/staticresources/_scripts/iconmap_json.py
@@ -0,0 +1,34 @@
+from lxml import etree
+import json
+
+FILES = [
+    r"../profiles/default/registry/icons_bootstrap.xml",
+    r"../profiles/default/registry/icons_contenttype.xml",
+    r"../profiles/default/registry/icons_country_flags.xml",
+    r"../profiles/default/registry/icons_language_flags.xml",
+    r"../profiles/default/registry/icons_mimetype.xml",
+    r"../profiles/default/registry/icons_plone.xml",
+    r"../profiles/default/registry/icons_toolbar.xml",
+]
+
+PATH_ICONMAP = "../static/iconmap.json"
+
+
+def main():
+    iconmap = []
+    for file in FILES:
+        root = etree.parse(file)
+        for record in root.xpath("//record"):
+            # fmt: off
+            iconmap.append({
+                "name": record.get("name"),
+                "value": record.xpath("value")[0].text
+            })
+            # fmt: on
+
+    with open(PATH_ICONMAP, "w") as f:
+        f.write(json.dumps(iconmap, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
