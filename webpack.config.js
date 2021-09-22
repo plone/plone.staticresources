f8d169942363c90db49864d4bbbbc9cc79f1b979
Johannes Raggam <thetetet@gmail.com>
2021-09-22 15:02:49 +0200

    PLIP 3211 - Pass the build base directory to the mockup config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const mockup_config = require("mockup/webpack.config.js");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = async (env, argv) => {
-    const config = await mockup_config(env, argv);
+    const config = await mockup_config(env, argv, __dirname);
 
     config.output.path = path.resolve(
         __dirname,
