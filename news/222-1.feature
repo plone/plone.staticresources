beeb2a10a3300d2e0c531a5afc041b526fd2c4a7
Johannes Raggam <thetetet@gmail.com>
2022-05-18 12:20:11 +0200

    Integrate bootstrap and jquery bundles with module federation. They now live in the bundle-plone directory.

---
+++ news/222-1.feature
@@ -0,0 +1,2 @@
+Integrate bootstrap and jquery bundles with module federation. They now live in the bundle-plone directory.
+[thet]
--- webpack.config.js
+++ webpack.config.js
@@ -15,16 +15,8 @@ module.exports = async (env, argv) => {
         new CopyPlugin({
             patterns: [
                 {
-                    from: path.resolve(__dirname, "node_modules/jquery/dist"),
-                    to: path.resolve(__dirname, "src/plone/staticresources/static/bundle-jquery"), // prettier-ignore
-                },
-                {
                     from: path.resolve(__dirname, "node_modules/bootstrap-icons/icons"),
                     to: path.resolve(__dirname, "src/plone/staticresources/static/icons-bootstrap"), // prettier-ignore
-                },
-                {
-                    from: path.resolve(__dirname, "node_modules/bootstrap/dist"),
-                    to: path.resolve(__dirname, "src/plone/staticresources/static/bundle-bootstrap"), // prettier-ignore
                 },
                 {
                     from: path.resolve(__dirname, "node_modules/svg-country-flags"),
