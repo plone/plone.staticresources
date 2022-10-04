db94666c4deb36e406c694bc3b2ad52da0450b1a
Johannes Raggam <thetetet@gmail.com>
2022-10-04 09:16:56 +0200

    Back to development: 2.0.0b9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plone.staticresources",
-    version="2.0.0b8",
+    version="2.0.0b9.dev0",
     description="Static resources for Plone",
     long_description=long_description,
     classifiers=[
