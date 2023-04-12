277be9d8b3dcd6d827af197663073df22dad8872
Peter Mathis <peter.mathis@kombinat.at>
2023-04-12 09:02:56 +0200

    Back to development: 2.0.10

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plone.staticresources",
-    version="2.0.9",
+    version="2.0.10.dev0",
     description="Static resources for Plone",
     long_description=long_description,
     classifiers=[
