67094753a80c8e4ba3bb07ae2520b8c50539babc
Peter Mathis <peter.mathis@kombinat.at>
2025-02-21 12:19:56 +0100

    Back to development: 2.2.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (
 
 setup(
     name="plone.staticresources",
-    version="2.2.3",
+    version="2.2.4.dev0",
     description="Static resources for Plone",
     long_description=long_description,
     long_description_content_type="text/x-rst",
